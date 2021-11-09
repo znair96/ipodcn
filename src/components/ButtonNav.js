6aeba686edb3a8cf8db700dc46914ec2a1330e66
= <zubnair2396@gmail.com>
2021-11-09 10:21:19 +0530

    added event for touch (zingtouch) commit

--- src/components/ButtonNav.js
+++ src/components/ButtonNav.js
@@ -1,17 +1,61 @@
 import React, { Component } from "react";
 import "../styles/buttonnav.css";
+import ZingTouch from "zingtouch";
 export default class ButtonNav extends Component {
-  mouseMoveEvent(event) {
-    let element = event.target.className;
-    if (
-      element === "forward" ||
-      element === "backward" ||
-      element === "nav-options"
-    ) {
-      console.log("Event Triggered");
-    }
+  // mouseMoveEvent(event) {
+  //   let eventTarget = event.target.className;
+  //   let element = document.getElementsByClassName("nav-options")[0];
+  //   if (eventTarget === "forward") {
+  //     element.addEventListener(
+  //       "mouseover",
+  //       (event) => {
+  //         event.stopPropagation();
+  //         // console.log(event);
+  //         if (document.getElementById("cover-flow").className === "highlight") {
+  //           document.getElementById("cover-flow").classList.remove("highlight");
+  //           document.getElementById("music").classList.add("highlight");
+  //         } else if (
+  //           document.getElementById("music").className === "highlight"
+  //         ) {
+  //           document.getElementById("music").classList.remove("highlight");
+  //           document.getElementById("games").classList.add("highlight");
+  //         } else if (
+  //           document.getElementById("games").className === "highlight"
+  //         ) {
+  //           document.getElementById("games").classList.remove("highlight");
+  //           document.getElementById("settings").classList.add("highlight");
+  //         } else if (
+  //           document.getElementById("settings").className === "highlight"
+  //         ) {
+  //           document.getElementById("settings").classList.remove("highlight");
+  //           document.getElementById("cover-flow").classList.add("highlight");
+  //         }
+  //       },
+  //       false
+  //     );
+  //   }
+  // }
+  componentDidMount() {
+    let element = document.getElementsByClassName("nav-options")[0];
+    let activeRegion = new ZingTouch.Region(element);
+    activeRegion.bind(element, "pan", (event) => {
+      if (document.getElementById("cover-flow").className === "highlight") {
+        document.getElementById("cover-flow").classList.remove("highlight");
+        document.getElementById("music").classList.add("highlight");
+      } else if (document.getElementById("music").className === "highlight") {
+        document.getElementById("music").classList.remove("highlight");
+        document.getElementById("games").classList.add("highlight");
+      } else if (document.getElementById("games").className === "highlight") {
+        document.getElementById("games").classList.remove("highlight");
+        document.getElementById("settings").classList.add("highlight");
+      } else if (
+        document.getElementById("settings").className === "highlight"
+      ) {
+        document.getElementById("settings").classList.remove("highlight");
+        document.getElementById("cover-flow").classList.add("highlight");
+      }
+    });
   }
-
   render() {
     return (
       <div
@@ -23,7 +67,7 @@ export default class ButtonNav extends Component {
         }}
       >
         <div className="outer-div">
-          <div className="nav-options" onMouseMove={this.mouseMoveEvent}>
+          <div className="nav-options">
             <div id="fbm">
               <p className="menu">MENU</p>
               <p className="forward">
--- src/components/Screen.js
+++ src/components/Screen.js
@@ -6,15 +6,30 @@ export default class Screen extends Component {
       <div className="screen">
         <div className="nav-links">
           <h2>ipod.js</h2>
-          <p className="highlight">
+          <p id="cover-flow" className="highlight">
             Cover Flow{" "}
             <span className="hidden">
               <i className="fas fa-chevron-right"></i>
             </span>
           </p>
-          <p>Music</p>
-          <p>Games</p>
-          <p>Settings</p>
+          <p id="music">
+            Music
+            <span className="hidden">
+              <i className="fas fa-chevron-right"></i>
+            </span>
+          </p>
+          <p id="games">
+            Games
+            <span className="hidden">
+              <i className="fas fa-chevron-right"></i>
+            </span>
+          </p>
+          <p id="settings">
+            Settings
+            <span className="hidden">
+              <i className="fas fa-chevron-right"></i>
+            </span>
+          </p>
         </div>
       </div>
     );
