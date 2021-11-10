411e540f5d570d080d6100f02be55b020da7449d
= <zubnair2396@gmail.com>
2021-11-10 10:02:07 +0530

    customised pan event commit

--- src/components/ButtonNav.js
+++ src/components/ButtonNav.js
@@ -2,43 +2,16 @@ import React, { Component } from "react";
 import "../styles/buttonnav.css";
 import ZingTouch from "zingtouch";
 export default class ButtonNav extends Component {
-  // mouseMoveEvent(event) {
-  //   let eventTarget = event.target.className;
-  //   let element = document.getElementsByClassName("nav-options")[0];
-  //   if (eventTarget === "forward") {
-  //     element.addEventListener(
-  //       "mouseover",
-  //       (event) => {
-  //         event.stopPropagation();
-  //         // console.log(event);
-  //         if (document.getElementById("cover-flow").className === "highlight") {
-  //           document.getElementById("cover-flow").classList.remove("highlight");
-  //           document.getElementById("music").classList.add("highlight");
-  //         } else if (
-  //           document.getElementById("music").className === "highlight"
-  //         ) {
-  //           document.getElementById("music").classList.remove("highlight");
-  //           document.getElementById("games").classList.add("highlight");
-  //         } else if (
-  //           document.getElementById("games").className === "highlight"
-  //         ) {
-  //           document.getElementById("games").classList.remove("highlight");
-  //           document.getElementById("settings").classList.add("highlight");
-  //         } else if (
-  //           document.getElementById("settings").className === "highlight"
-  //         ) {
-  //           document.getElementById("settings").classList.remove("highlight");
-  //           document.getElementById("cover-flow").classList.add("highlight");
-  //         }
-  //       },
-  //       false
-  //     );
-  //   }
-  // }
   componentDidMount() {
     let element = document.getElementsByClassName("nav-options")[0];
     let activeRegion = new ZingTouch.Region(element);
-    activeRegion.bind(element, "pan", (event) => {
+    //Customised Pan event
+    let customPan = new ZingTouch.Pan({
+      threshold: 15,
+      numInputs: 1,
+    });
+    //Binding the event in the region
+    activeRegion.bind(element, customPan, (event) => {
       if (document.getElementById("cover-flow").className === "highlight") {
         document.getElementById("cover-flow").classList.remove("highlight");
         document.getElementById("music").classList.add("highlight");
