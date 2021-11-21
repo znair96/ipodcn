7d667a667605160b874e1ea599c517366bff1c9f
= <zubnair2396@gmail.com>
2021-11-21 23:20:32 +0530

    css change

--- src/components/CoverFlow.js
+++ src/components/CoverFlow.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function CoverFlow() {
   return (
-    <div className="games-section">
+    <div className="coverflow">
       <h2>Coverflow</h2>
     </div>
   );
