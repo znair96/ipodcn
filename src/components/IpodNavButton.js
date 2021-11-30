7cd97f370fcc7f921ffc8279e1604e97519fc164
= <zubnair2396@gmail.com>
2021-11-30 23:21:59 +0530

    solved the music navigation

--- src/components/IpodNavButton.js
+++ src/components/IpodNavButton.js
@@ -2,21 +2,23 @@ import React, { Component } from "react";
 import ZingTouch from "zingtouch";
 export default class IpodNavButton extends Component {
   componentDidMount() {
-    console.log(this.props.currentMenu + " from mount");
-    let element = document.getElementsByClassName("nav-options")[0];
+    let element = document.querySelector(".nav-options");
     let activeRegion = new ZingTouch.Region(element);
     //Customised Pan event
     let customPan = new ZingTouch.Pan({
       threshold: 25,
       numInputs: 1,
     });
-    //Binding the event in the region
-    let menu = ["cover-flow", "music", "games", "settings"];
-    //   const musicOptions = ["all-songs", "artists", "albums"];
-
     activeRegion.bind(element, customPan, (event) => {
+      let menu;
+      if (this.props.currentMenu === "Music") {
+        menu = ["all-songs", "artists", "albums"];
+      } else {
+        menu = ["cover-flow", "music", "games", "settings"];
+      }
       const id = document.querySelector(".highlight").id;
       const fetchIndexOfMenu = menu.indexOf(id);
+
       if (fetchIndexOfMenu !== menu.length - 1) {
         document
           .getElementById(menu[fetchIndexOfMenu])
@@ -30,48 +32,9 @@ export default class IpodNavButton extends Component {
           .classList.remove("highlight");
         document.getElementById(menu[0]).classList.add("highlight");
       }
+      console.log(id, fetchIndexOfMenu, this.props.currentMenu);
     });
   }
-  // componentDidUpdate() {
-  //   // console.log("I am in music componentdidmount");
-  //   debugger;
-  //   console.log(this.props.currentMenu + " from update");
-  //   let menu;
-  //   if (this.props.currentMenu === "Music") {
-  //     menu = ["all-songs", "artists", "albums"];
-  //   } else {
-  //     menu = ["cover-flow", "music", "games", "settings"];
-  //   }
-  //   let element = document.getElementsByClassName("nav-options")[0];
-  //   let activeRegion = new ZingTouch.Region(element);
-  //   //Customised Pan event
-  //   let customPan = new ZingTouch.Pan({
-  //     threshold: 25,
-  //     numInputs: 1,
-  //   });
-  //   //Binding the event in the region
-  //   // let menu = ["cover-flow", "music", "games", "settings"];
-  //   // const menu = ["all-songs", "artists", "albums"];
-  //   activeRegion.bind(element, customPan, (event) => {
-  //     debugger;
-  //     const id = document.querySelector(".highlight").id;
-  //     const fetchIndexOfMenu = menu.indexOf(id);
-  //     console.log(fetchIndexOfMenu, id);
-  //     if (fetchIndexOfMenu !== menu.length - 1) {
-  //       document
-  //         .getElementById(menu[fetchIndexOfMenu])
-  //         .classList.remove("highlight");
-  //       document
-  //         .getElementById(menu[fetchIndexOfMenu + 1])
-  //         .classList.add("highlight");
-  //     } else {
-  //       document
-  //         .getElementById(menu[fetchIndexOfMenu])
-  //         .classList.remove("highlight");
-  //       document.getElementById(menu[0]).classList.add("highlight");
-  //     }
-  //   });
-  // }
   render() {
     //  const event = (this.props.currentMenu!=="Menu") ? "" : `onClick=${this.props.menuSelect}`;
     if (
--- src/components/Music.js
+++ src/components/Music.js
@@ -1,46 +1,6 @@
 import React, { Component } from "react";
-import ZingTouch from "zingtouch";
+// import ZingTouch from "zingtouch";
 export default class Music extends Component {
-  componentDidMount() {
-    // console.log("I am in music componentdidmount");
-    // debugger;
-    // console.log(this.props.currentMenu + " from update");
-    let menu;
-    if (this.props.currentMenu === "Music") {
-      menu = ["all-songs", "artists", "albums"];
-    } else {
-      menu = ["cover-flow", "music", "games", "settings"];
-    }
-    let element = document.getElementsByClassName("nav-options")[0];
-    let activeRegion = new ZingTouch.Region(element);
-    //Customised Pan event
-    let customPan = new ZingTouch.Pan({
-      threshold: 25,
-      numInputs: 1,
-    });
-    //Binding the event in the region
-    // let menu = ["cover-flow", "music", "games", "settings"];
-    // const menu = ["all-songs", "artists", "albums"];
-    activeRegion.bind(element, customPan, (event) => {
-      // debugger;
-      const id = document.querySelector(".highlight").id;
-      const fetchIndexOfMenu = menu.indexOf(id);
-      console.log(fetchIndexOfMenu, id);
-      if (fetchIndexOfMenu !== menu.length - 1) {
-        document
-          .getElementById(menu[fetchIndexOfMenu])
-          .classList.remove("highlight");
-        document
-          .getElementById(menu[fetchIndexOfMenu + 1])
-          .classList.add("highlight");
-      } else {
-        document
-          .getElementById(menu[fetchIndexOfMenu])
-          .classList.remove("highlight");
-        document.getElementById(menu[0]).classList.add("highlight");
-      }
-    });
-  }
   render() {
     return (
       <div className="nav-links">
